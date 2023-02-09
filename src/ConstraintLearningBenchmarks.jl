14f731fb5fbceb991cb18687b5937b4ab6a936be
azzaare <jf@baffier.fr>
2023-02-09 18:17:26 +0900

    move old code. Add param code

--- src/ConstraintLearningBenchmarks.jl
+++ src/ConstraintLearningBenchmarks.jl
@@ -1,44 +1,41 @@
 module ConstraintLearningBenchmarks
 
 # usings
-using BenchmarkTools
+# using BenchmarkTools
+using ConstraintCommons
 using ConstraintDomains
 using ConstraintLearning
 using Constraints
-using CSV
-using DataFrames
-using DataVoyager
-using Dictionaries
-using Distributed
+# using CSV
+# using DataFrames
+# using DataVoyager
+# using Dictionaries
+# using Distributed
 using DrWatson
-using JSON
-using Statistics
-using StatsBase
-using Tables
-#using SharedArrays
-
-# imports
-import Constraints: make_error
+# using JSON
+# using Statistics
+# using StatsBase
+# using Tables
 
 # constants
-export ALL_PARAMETERS
-export BENCHED_CONSTRAINTS
+# export ALL_PARAMETERS
+# export BENCHED_CONSTRAINTS
 
 # others
-export analyse_composition
-export analyze_icn
-export compositions_benchmark
-export icn_benchmark
-export search_space
-export visualize_compositions
-export visualize_icn
+# export analyse_composition
+# export analyze_icn
+# export compositions_benchmark
+# export icn_benchmark
+# export search_space
+# export visualize_compositions
+# export visualize_icn
 
 # includes
 include("constants.jl")
-include("search_space.jl")
-include("extra_constraints.jl")
-include("icn.jl")
-include("composition.jl")
-include("analyze.jl")
+# include("search_space.jl")
+# include("extra_constraints.jl")
+# include("icn.jl")
+# include("composition.jl")
+# include("analyze.jl")
 
 end
